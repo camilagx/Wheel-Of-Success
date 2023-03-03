7b902665ade860b6370bfea54282132fa21ec175
camilagx <camilac.gomez97@gmail.com>
2023-03-03 13:58:11 -0700

    refacted findBtn function

--- scripts/app.js
+++ scripts/app.js
@@ -14,40 +14,56 @@ startBtn.addEventListener('click', () => {
 });
 
 document.addEventListener('keydown', (e) => {
-  
-
   //get the key entered by user
   const keyVal = e.key
-
-  //find button with matching key 
-
-  //set that button to class name 'chosen'
-
-  //set that button's attribute to disabled
-  
+  //find button, set btn class name to 'chosen' and disable btn
+  findBtn(keyVal);
   //invoke checkLetter function and store return value to variable letterFound
-
+  const letterFound = checkLetter(keyVal);
 });
 
-function findDiv(letter) {
-  /*
-  const keyRowDiv_1 = qwerty.firstElementChild;
-  const keyRowDiv_2 = keyRowDiv_1.nextElementSibling;
-  const keyRowDiv_3 = keyRowDiv_2.nextElementSibling;
-*/
+function findBtn(keyVal){
+  const divOne = qwerty.firstElementChild;
+  const divTwo = divOne.nextElementSibling;
+  const divThree= divTwo.nextElementSibling;
+  const divs = [divOne, divTwo, divThree];
 
-  let keyRowDiv = '';
-  const keyRow = {
-    divOne : ['q','w','e','r','t','y','u','i','o','p'],
-    divTwo : ['a','s','d','f','g','h','j','k', 'l'],
-    divThree : ['z','x','c','v','b','n','m']
-  }
+  divs.forEach((div) => {
+    const buttons = div.children;
+    for(let btn of buttons){
+      if(btn.textContent === keyVal){
+        btn.className = 'chosen';
+        btn.disabled = true;
+      }
+    }
+  })
+}
 
-  for (let div in keyRow){
-    if (keyRow[div].includes(letter)) {
-      keyRowDiv = div;
+function checkLetter(keyVal){
+  const lis = phraseUL.children;
+  let letter = null;
+
+  for(let li of lis){
+    if (li.textContent === keyVal){
+      li.className = 'show';
+      letter = li.textContent;
     }
   }
+  return letter;
+}
+    
+function addPhraseToDisplay(arr){
+  arr.forEach(x => {
+    const li = document.createElement('li');
+    li.textContent = x;
+    li.className = 'letter';
+    phraseUL.appendChild(li);
+  })
+}
+
+function getRandomPhraseAsArray(phrases){
+  const index = Math.floor(Math.random() * phrases.length);
+  return phrases[index].split('');
 }
 
 //API Call for random words
@@ -66,36 +82,10 @@ fetch(url, options)
   .then(data => {
     const phrases = data;
     const phrase = getRandomPhraseAsArray(phrases);
+    
     console.log(phrase);
     addPhraseToDisplay(phrase);
-
     })
     .catch(err => console.error(err));
 
-function checkLetter(btn){
-  const lis = phraseUL.children;
-  let letter = null;
-
-  lis.forEach((li, index) => {
-    if (btn.textContent === li.textContent){
-      li.className = 'show';
-      letter= li.textContent;
-    }
-  })
-
-  return letter;
-}
-    
-function addPhraseToDisplay(arr){
-  arr.forEach(x => {
-    const li = document.createElement('li');
-    li.textContent = x;
-    li.className = 'letter';
-    phraseUL.appendChild(li);
-  })
-}
-
-function getRandomPhraseAsArray(phrases){
-  const index = Math.floor(Math.random() * phrases.length);
-  return phrases[index].split('');
-}
+ 
